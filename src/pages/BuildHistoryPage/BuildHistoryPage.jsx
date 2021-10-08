be5eb2d0cc6912c202ccf6f3a8a4778742d07f9c
enmalafeev <malafeev.en@yandex.ru>
2021-10-08 15:59:19 +0300

    done BuildHistory component

--- src/pages/BuildHistoryPage/BuildHistoryPage.jsx
+++ src/pages/BuildHistoryPage/BuildHistoryPage.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './buildHistory.css';
 import Footer from '../../components/Footer/Footer';
-import BuildHeader from '../../components/Button/BuildHeader/BuildHeader';
+import BuildHeader from '../../components/BuildHeader/BuildHeader';
+import BuildCard from '../../components/BuildCard/BuildCard';
+import Button from '../../components/Button/Button';
 
 function BuildHistoryPage() {
   return (
     <div className="wrapper">
       <BuildHeader />
       <main className="build-content">
-        main content
+        <div className="build-container">
+          {Array.from({length: 10}, (_, i) => i + 1).map((item, index) => <BuildCard key={index} />)}
+          <div className="build-btn-group">
+            <Button type="accent" size="small">
+              <span>Show more</span>
+            </Button>
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
