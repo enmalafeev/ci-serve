e17ba480b6c4968474b231401d4778a7d7a7af58
enmalafeev <malafeev.en@yandex.ru>
2021-10-09 00:34:06 +0300

    add close icon for Input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './input.css';
+import {ReactComponent as CloseIcon} from '../../assets/svg/close.svg';
 
 function Input({ label, placeholder, required }) {
+  const [inputValue, setInputValue] = useState('');
+  const onChange = (event) => setInputValue(event.target.value);
+  const clearHandle = () => setInputValue('');
   return ( 
     <div className="input-wrapper">
-      <label className="input__label" htmlFor="repo">{label}</label>
-      <input className="input__field" type="text" placeholder={placeholder} id="repo" required={required} />
+      <label className="input__label" htmlFor="repo">
+        {label}
+        {inputValue.length > 0 && <div className="input__close-icon" onClick={clearHandle}>
+          <CloseIcon />
+        </div>}
+      </label>
+      <input
+        id="repo"
+        type="text"
+        value={inputValue}
+        className="input__field"
+        placeholder={placeholder}
+        required={required}
+        onChange={onChange} />
     </div>
   );
 }
